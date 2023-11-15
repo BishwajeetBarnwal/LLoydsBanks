c37f9aca08f5dcdcfaac7725759ea271fe6b68e6
Bishwajeet Kumar <barnwalbishwa@gmail.com>
2023-11-15 12:19:06 +0000

    Update OptionalParameter.java

--- com.talkingclocks/src/main/java/com/llyods/com/talkingclocks/OptionalParameter.java
+++ com.talkingclocks/src/main/java/com/llyods/com/talkingclocks/OptionalParameter.java
@@ -1,5 +1,14 @@
 package com.llyods.com.talkingclocks;
 
-public class OptionalParameter {
+	import org.springframework.web.bind.annotation.GetMapping;
+	import org.springframework.web.bind.annotation.PathVariable;
+	import org.springframework.web.bind.annotation.RestController;
 
+	@RestController
+	public class OptionalParameter {
+		
+		@GetMapping(path = "/Talking-Clock/path-variable/{id}")
+		public TalkingClock talkingClockPathVariable(@PathVariable String id) {
+			return new TalkingClock(String.format(id , "O' Clock"));
+		}
 }
