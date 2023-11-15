a88728a9d5f5e5c12280c3e7a0d05619f145714d
Bishwajeet Kumar <barnwalbishwa@gmail.com>
2023-11-15 12:37:49 +0000

    Update HumanFriendlyTest.java

--- com.talkingclocks/src/test/java/com/llyods/com/talkingclocks/HumanFriendlyTest.java
+++ com.talkingclocks/src/test/java/com/llyods/com/talkingclocks/HumanFriendlyTest.java
@@ -7,8 +7,12 @@ import org.junit.Test;
 public class HumanFriendlyTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void TalkingClockprintwordstest() {
+		
+		StringClock clock = new StringClock();
+		
+		assertEquals("1", clock.checkclockisequals("1"));
+	
 	}
 
 }
