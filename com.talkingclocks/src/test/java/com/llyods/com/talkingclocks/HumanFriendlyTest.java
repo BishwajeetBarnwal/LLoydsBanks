e5e4434a60d2859c3d7f0c369435ad6154b59e22
Bishwajeet Kumar <barnwalbishwa@gmail.com>
2023-11-15 12:20:55 +0000

    Add files via upload

---
+++ com.talkingclocks/src/test/java/com/llyods/com/talkingclocks/HumanFriendlyTest.java
@@ -0,0 +1,14 @@
+package com.llyods.com.talkingclocks;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HumanFriendlyTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
