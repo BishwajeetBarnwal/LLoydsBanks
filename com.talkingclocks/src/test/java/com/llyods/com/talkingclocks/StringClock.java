eb3972972dcad67f0fee2cfa06f0d83fc3caee06
Bishwajeet Kumar <barnwalbishwa@gmail.com>
2023-11-15 12:38:08 +0000

    Add files via upload

---
+++ com.talkingclocks/src/test/java/com/llyods/com/talkingclocks/StringClock.java
@@ -0,0 +1,10 @@
+package com.llyods.com.talkingclocks;
+
+public class StringClock {
+
+	public Object checkclockisequals(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
